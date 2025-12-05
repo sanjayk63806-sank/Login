871b8f17e927165693c9829d3ce5e3c80f2ba4fa
sanjayk63806-sank <sanjayk63806@gmail.com>
2025-12-05 15:38:20 +0530

    Add files via upload
    day 2 Login

---
+++ FacebookLogin.java
@@ -0,0 +1,17 @@
+package learnautomation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FacebookLogin {
+
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https:\\www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("sanjay@facebook.com");
+		driver.findElement(By.id("pass")).sendKeys("sample password");
+		driver.findElement(By.name("login")).click();
+
+	}
+
+}
